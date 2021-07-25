fac8aeb7e96c99233a9ee1fe5b2d313dd67b02d5
valen2510 <valentina.jaramillo@imagineapps.co>
2021-07-25 13:16:18 -0500

    Add interview peparation task Lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    """ unlock boxes function"""
+
+    if len(boxes) == 0:
+        return False
+
+    unlock_boxes = [0]
+    for key in unlock_boxes:
+        for element in boxes[key]:
+            if element not in unlock_boxes and element < len(boxes):
+                unlock_boxes.append(element)
+
+    if len(unlock_boxes) == len(boxes):
+        return True
+    return False
